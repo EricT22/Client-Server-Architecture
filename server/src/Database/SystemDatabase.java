723b2471d25f4e973f8be6b4b2dfec8d45188e17
Scott Wegley <scbrwegley@protonmail.ch>
2023-11-30 10:45:36 -0800

    fixed templating errors

--- server/src/Database/SystemDatabase.java
+++ server/src/Database/SystemDatabase.java
@@ -1,7 +1,13 @@
 package Database;
 
 public class SystemDatabase extends AbstractSQLConnector{
+    public SystemDatabase(String username, String password) {
+        super(username, password);
+        //TODO Auto-generated constructor stub
+    }
+
     public String getImage(){
+        return null;
 
     }    
 }
