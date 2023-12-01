cbac2e71522e698e39bf4518cc72b2de8d727a50
scottwegley <scottwegley@gmail.com>
2023-12-01 13:15:15 -0800

    setup api server only login path works currently

--- server/src/Admin/Server.java
+++ server/src/Admin/Server.java
@@ -8,6 +8,7 @@ import Database.UserDatabase;
 
 import java.io.IOError;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import com.sun.net.httpserver.HttpServer;
@@ -15,7 +16,7 @@ import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.BasicAuthenticator;
 
 public class Server implements Runnable {
-    private static final int PORT = 0; // idk change later
+    private static final int PORT = 8000;
 
     private static UserDatabase userDB;
     private static SystemDatabase systemDB;
@@ -28,25 +29,66 @@ public class Server implements Runnable {
 
     private int nextID;
 
-    public Server() throws Exception {
-        configureServer();
+    public Server() {
+
     }
 
     public void configureServer() throws IOException {
-        ArrayList<HttpContext> contexts = new ArrayList<HttpContext>();
-        APIServer = HttpServer.create(new InetSocketAddress(PORT),0);
+        ArrayList<HttpContext> secureContexts = new ArrayList<HttpContext>();
+        APIServer = HttpServer.create(new InetSocketAddress(PORT), 0);
 
-        contexts.forEach((c) -> c.setAuthenticator(new BasicAuthenticator() {
+        APIServer.createContext("/api/login", (exchange -> {
+            if(!"POST".equals(exchange.getRequestMethod())){
+                exchange.sendResponseHeaders(405, -1);
+            } else {
+                String responseText = "Logged in successfully";
+                exchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(responseText.getBytes());
+                output.flush();
+            }
+            exchange.close();
+        }));
+
+        APIServer.createContext("/api/recovery", (exchange -> {
 
         }));
+
+        secureContexts.add(APIServer.createContext("/api/write", (exchange -> {
+
+        })));
+
+        secureContexts.add(APIServer.createContext("/api/read", (exchange -> {
+
+        })));
+
+        secureContexts.add(APIServer.createContext("/api/logout", (exchange -> {
+
+        })));
+
+        secureContexts.forEach((c) -> c.setAuthenticator(new BasicAuthenticator("pwdProtected") {
+            @Override
+            public boolean checkCredentials(String username, String password) {
+                // TODO Implement checking user db for user and corresponding password
+                return true;
+            };
+        }));
+
+        APIServer.setExecutor(null);
+        APIServer.start();
     }
 
     @Override
     public void run() {
-
+        try {
+            configureServer();
+            System.out.println("API Server is online at " + APIServer.getAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void stop() {
-
+        APIServer.stop(0);
     }
 }
