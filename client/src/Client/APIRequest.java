bd3e78486eecac102f00bf5e2053cee72b8e8542
scottwegley <scottwegley@gmail.com>
2023-12-01 14:56:55 -0800

    implemented api request class currently no headers are sent, this will be remedied later

--- client/src/Client/APIRequest.java
+++ client/src/Client/APIRequest.java
@@ -1,33 +1,78 @@
 package Client;
 
+import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class APIRequest {
-    private String serverIP;
+    private static String serverIP;
     private RequestScheme scheme;
     private String payload;
-    private String response;
-    private HttpRequest request;
+    private CompletableFuture<HttpResponse<String>> response = null;
+    private HttpRequest request = null;
 
-    public void setIP(String ip){
-
+    public static void setIP(String ip) {
+        serverIP = ip;
     }
 
-    public APIRequest makeRequest(RequestScheme scheme, String request){
-        // delete this when done
-        throw new UnsupportedOperationException();
+    public static APIRequest makeRequest(RequestScheme scheme, String data) {
+        APIRequest apiReq = new APIRequest();
+        switch (scheme) {
+            case LOGIN:
+                apiReq.request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://" + serverIP + "/api/login"))
+                        .method("POST", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                break;
+            case ACCT_RECOVERY:
+                apiReq.request = HttpRequest.newBuilder()
+                        .uri(URI.create(serverIP + "/api/recovery"))
+                        .method("POST", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                break;
+            case READ_DATA:
+                apiReq.request = HttpRequest.newBuilder()
+                        .uri(URI.create(serverIP + "/api/login"))
+                        .method("GET", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                break;
+            case WRITE_DATA:
+                apiReq.request = HttpRequest.newBuilder()
+                        .uri(URI.create(serverIP + "/api/login"))
+                        .method("POST", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                break;
+            case LOGOUT:
+                apiReq.request = HttpRequest.newBuilder()
+                        .uri(URI.create(serverIP + "/api/login"))
+                        .method("POST", HttpRequest.BodyPublishers.noBody())
+                        .build();
+                break;
+            default:
+                break;
+        }
+        return apiReq;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "";
     }
 
-    public boolean execute(){
-        return false;
+    public boolean execute() throws InterruptedException, ExecutionException {
+        System.out.println(request.toString());
+        if (request == null) {
+            return false;
+        }
+        response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.get().body());
+        return response.get().statusCode() == 200;
     }
 
-    public String getResponse(){
+    public String getResponse() {
         return "";
     }
 }
