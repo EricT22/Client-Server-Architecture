6e8cbb24adeed2112f64a6080fcb87331bdac6c4
eric <phantomenace87@gmail.com>
2023-12-03 18:00:38 -0800

    deleted unused gui import

--- client/src/Client/ClientGUI.java
+++ client/src/Client/ClientGUI.java
@@ -21,7 +21,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
 
 import java.util.regex.Pattern;
 
