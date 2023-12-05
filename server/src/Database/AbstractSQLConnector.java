3a20d34f07692d02b017a0894347ed6ef04b47e3
Scott Wegley <scbrwegley@protonmail.ch>
2023-12-05 10:13:52 -0800

    formatting

--- server/src/Database/AbstractSQLConnector.java
+++ server/src/Database/AbstractSQLConnector.java
@@ -6,36 +6,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-public abstract class AbstractSQLConnector implements DatabaseInterface{
+public abstract class AbstractSQLConnector implements DatabaseInterface {
     protected Connection connection = null;
     protected Statement statement = null;
     protected ResultSet resultSet = null;
     protected String url = "jdbc:mysql://127.0.0.1:3306/clientServerApp";
 
+    public AbstractSQLConnector(String username, String pass) {
 
-    public AbstractSQLConnector(String username, String pass){
+        try {
+            connection = DriverManager.getConnection(url, username, pass);
 
-    try {
-    connection = DriverManager.getConnection(url, username, pass);
+            statement = connection.createStatement();
 
-    statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT VERSION()");
+            if (resultSet.next()) {
 
-    resultSet = statement.executeQuery("SELECT VERSION()");
-    if (resultSet.next()){
+                System.out.println("MySQL version: " + resultSet.getString(1) + "\n=====================\n");
+            }
 
-        System.out.println("MySQL version: " + resultSet.getString(1) + "\n=====================\n");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
     }
 
-
-    }
-    catch(SQLException ex) {
-    System.out.println("SQLException: " + ex.getMessage());
-    System.out.println("SQLState: " + ex.getSQLState());
-    System.out.println("VendorError: " + ex.getErrorCode());
-    }
-
-}
     public abstract void updateDatabase();
 
     public abstract String getResultSet();
