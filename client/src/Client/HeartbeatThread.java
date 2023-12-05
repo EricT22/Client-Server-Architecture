c1bb4b148c7cf0616415e46d21008eb2cdf3e96b
scottwegley <scottwegley@gmail.com>
2023-12-04 23:50:18 -0800

    rudimentary heartbeat thread

--- client/src/Client/HeartbeatThread.java
+++ client/src/Client/HeartbeatThread.java
@@ -1,22 +1,34 @@
 package Client;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class HeartbeatThread extends Thread{
+public class HeartbeatThread extends Thread {
     private AtomicBoolean active = new AtomicBoolean(false);
     private Socket socket;
-    
-    public HeartbeatThread(Socket iSocket){
+
+    public HeartbeatThread(Socket iSocket) {
         this.socket = iSocket;
     }
 
     @Override
-    public void run(){
+    public void run() {
+        active.set(true);
 
+        while (active.get()) {
+            try {
+                (new DataOutputStream(socket.getOutputStream())).writeInt(1);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void stopHeartbeat(){
+    public void stopHeartbeat() {
+        active.set(false);
         try {
             socket.close();
         } catch (Exception e) {
