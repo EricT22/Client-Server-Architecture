798b30aa5704b80005bdf208c56fa346832b6767
Scott Wegley <scbrwegley@protonmail.ch>
2023-12-05 09:47:37 -0800

    created client thread loopo to receive heatbeat

--- server/src/Admin/ClientThread.java
+++ server/src/Admin/ClientThread.java
@@ -1,32 +1,51 @@
 package Admin;
 
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ClientThread extends Thread{
+public class ClientThread extends Thread {
     private int sessionID;
-    private boolean connected;
-    private long lastHeartbeat;
+    private long lastHeartbeat = -1;
     private Socket socket;
 
-    public ClientThread(int id, Socket iSocket){
+    private AtomicBoolean active = new AtomicBoolean(false);
+
+    public ClientThread(int id, Socket iSocket) throws SocketException {
         sessionID = id;
         socket = iSocket;
+        socket.setSoTimeout(300000);
     }
 
-    public Socket getSocket(){
-        return socket;
-    }
-
-    public void stopClient(){
-
+    public void stopClient() throws IOException {
+        active.set(false);
+        socket.close();
     }
 
     @Override
-    public void run(){
+    public void run() {
+        active.set(true);
 
+        while (active.get()) {
+            try {
+                int status = (new DataInputStream(socket.getInputStream())).read();
+                lastHeartbeat = System.currentTimeMillis();
+            } catch (SocketTimeoutException e) {
+                active.set(false);
+                System.out.println("Client connection on ID" + sessionID + " timed out");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public long getTimeSinceSeen(){
-        return 0;
+    public long getTimeSinceSeen() {
+        if (lastHeartbeat == -1) {
+            lastHeartbeat = System.currentTimeMillis();
+        }
+        return System.currentTimeMillis() - lastHeartbeat;
     }
 }
